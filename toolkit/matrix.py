da0601d038ad17cc97fc40dc356bc05242d3810f
Hayden Parkinson <haydendparkinson@gmail.com>
2019-04-30 15:39:20 -0600

    Fixed re.search pattern to match arff standard

--- toolkit/matrix.py
+++ toolkit/matrix.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 import random
 import numpy as np
+import re
 
 def mode(a, axis=0):
 # taken from scipy code
@@ -103,7 +104,10 @@ class Matrix:
                             attr_name = attr_def[:attr_def.index("'")]
                             attr_def = attr_def[attr_def.index("'")+1:].strip()
                         else:
-                            search = re.search(r'(\w*)\s*({.*})', attr_def)
+                            # TODO: CHANGED PATTERN: SECOND GROUP NO LONGER
+                            # ONLY SEARCHES WITHIN IN BRACKETS
+                            search = re.search(r'(\w*)\s*(.*)', attr_def)
+                            print(attr_def, search, type(search))
                             attr_name = search.group(1)
                             attr_def = search.group(2)
                             # Remove white space from atribute values
